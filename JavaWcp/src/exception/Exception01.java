8f3571d7851b90241e4ddc300f207355c8df6753
taku-WC <pio.neer.bi10@gmail.com>
2020-11-15 22:30:16 +0900

    chapter10

--- JavaWcp/src/exception/Exception01.java
+++ JavaWcp/src/exception/Exception01.java
@@ -1,16 +1,12 @@
 package exception;
-
+//エラー発生
 public class Exception01 {
     public static void main(String[] args) {
-    	try {
-    		System.out.println("100 ÷ 0 は？");
-    		// 0で割り算するので、例外が発生
-    		int result = 100 / 0;
-    		System.out.println("計算結果" + result);
-    	} catch (ArithmeticException e) {
-    		System.out.println("例外が発生");
-    	} finally {
-    		System.out.println("プログラム終了");
-    	}
+        System.out.println("100 ÷ 0 は？");
+        // 0で割り算するので、例外が発生
+        int result = 100 / 0;
+        // 例外が発生した以降の処理は実行されない
+        System.out.println("計算結果" + result);
+        System.out.println("プログラム終了");
     }
 }
---
+++ JavaWcp/src/exception/Exception03.java
@@ -0,0 +1,31 @@
+package exception;
+
+public class Exception03 {
+	public static void main(String[] args) {
+		try {
+			// divisionメソッドを呼び出し
+			Exception03.division(100, 0);			
+		// ArithmeticExceptionクラスの例外が発生した場合の例外処理
+		} catch (ArithmeticException e) {
+			System.out.println("ArithmeticExceptionが発生");
+		
+		// IllegalArgumentExceptionクラスの例外が発生した場合の例外処理
+		} catch (IllegalArgumentException e) {
+			System.out.println("IllegalArgumentExceptionが発生");
+			// キャッチした例外インスタンスをスロー
+			throw e;
+		}
+        // finallyブロックでないので処理が実行されない
+        System.out.println("プログラム終了");
+	}
+	
+    public static void division(int a, int b) {
+        System.out.println(a + " ÷ " + b + " は？");
+        if (b == 0) {
+            // 割り算で例外が発生するので、意図的にIllegalArgumentException例外をスロー
+            throw new IllegalArgumentException("引数が不正です");
+        }
+        int result = a / b;
+        System.out.println("計算結果" + result);
+    }
+}
