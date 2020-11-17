b52b4d3ded95672589c7da625cf221e3827781d4
taku-WC <pio.neer.bi10@gmail.com>
2020-11-18 00:56:56 +0900

    chapter13

--- JavaWcp/src/exception/Exception04.java
+++ JavaWcp/src/exception/Exception04.java
@@ -12,11 +12,11 @@ public class Exception04 {
 		}
 		System.out.println("プログラム終了");
 	}
-	
+
 	public static void readFile(String fileName) throws FileNotFoundException{
 		System.out.println("ファイル読み込み開始");
-		FileReader fr= new FileReader(fileName);
+		FileReader fr = new FileReader(fileName);
 		System.out.println(fileName + "の読み込み完了");
-		
+
 	}
 }
