e9f3bcb07ea87c47b1170d4bbcaa7f47d5e7db26
taku-WC <pio.neer.bi10@gmail.com>
2020-12-25 18:30:41 +0900

    dojo1

--- JavaWcp/src/java3/Main.java
+++ JavaWcp/src/java3/Main.java
@@ -1,8 +1,37 @@
 package java3;
 
-public class Main {
-	  public static void main(String[] args) {
-	    Person.printData(Person.fullName("Kate", "Jones"), 27, 1.6, 50.0);
-	    Person.printData(Person.fullName("John", "Christopher", "Smith"), 65, 1.75, 80.0);
-	  }
-	}
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("何人分の情報を入力しますか：");
+    int number = scanner.nextInt();
+    for(int i = 0; i < number; i++){
+      System.out.println(i + 1 + "人目");
+
+      System.out.println("名前：");
+      String firstName = scanner.next();
+
+      System.out.println("名字：");
+      String lastName = scanner.next();
+
+      System.out.println("年齢：");
+      int age = scanner.nextInt();
+
+      System.out.println("身長(m)：");
+      double height = scanner.nextDouble();
+
+      System.out.println("体重(kg)：");
+      double weight = scanner.nextDouble();
+
+
+
+      Person.printData(firstName, lastName, age, height, weight);
+    }
+
+  }
+
+
+}
+
